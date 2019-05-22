11cce10529aa2783b3199a582229c249aeb90463
prachi <prachi@DESKTOP-NU2SNQT>
2019-05-22 09:45:59 -0400

    Initial Commit

---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,15 @@
+
+package studentweek3;
+
+
+public class StudentWeek3 {
+
+   
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setFirstname("John");
+        s1.setId(4);
+        
+    }
+    
+}
