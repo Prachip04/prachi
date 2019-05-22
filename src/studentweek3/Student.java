589f6d34e2dfa56d6086b970d9ffcf7593f1e515
prachi <prachi@DESKTOP-NU2SNQT>
2019-05-22 10:13:54 -0400

    Initial Commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,36 @@
+package studentweek3;
+
+
+public class Student {
+   
+    private int id;
+    private String Firstname;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the Firstname
+     */
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    /**
+     * @param Firstname the Firstname to set
+     */
+    public void setFirstname(String Firstname) {
+        this.Firstname = Firstname;
+    }
+}
